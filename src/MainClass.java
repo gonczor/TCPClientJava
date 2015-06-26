42b0bcf97df7faf255a6ce2d1b8d64da69238637
gonczor <wiktor.gonczaronek@gmail.com>
2015-06-26 16:34:30 +0200

    Initial connection established

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,17 @@
+import java.net.*;
+
 public class MainClass {
+
+    static String ADDRESS = "127.0.0.1";
+
     public static void main(String[] args){
-        System.out.print("Starting out job.");
+        try{
+
+            Socket mySocket = new Socket(ADDRESS, 12345);
+            System.out.println(mySocket.getInetAddress());
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
