15373a3a963ed2da51c01db6cd3407421562eb93
gonczor <wiktor.gonczaronek@gmail.com>
2015-06-30 20:51:33 +0200

    DataOutputStream set to deal with connections with python server

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,8 +4,6 @@ import ConnectionHandling.*;
 
 public class MainClass {
 
-    static String ADDRESS = "127.0.0.1";
-
     public static void main(String[] args){
 
         Connection connection = new Connection();
